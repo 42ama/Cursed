a6f82715c6a2af3da79ee5e1cb3f7443d42e9999
42ama <45532431+42ama@users.noreply.github.com>
2019-10-29 21:31:14 +0400

    Added new service - Product Catalog. Which implements IRESTAsync. Also added corespondidng controller and view

---
+++ Cursed/Models/Data/ProductCatalogDataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cursed.Models.Data
+{
+    public class ProductCatalogDataModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int? CAS { get; set; }
+        public string Type { get; set; }
+        public bool? LicenseRequired { get; set; }
+        public int? GovermentNum { get; set; }
+        public DateTime? LicensedUntil { get; set; }
+    }
+}
