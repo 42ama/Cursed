a831d40bb04ee86307041a9211fb198a9af767fd
42ama <45532431+42ama@users.noreply.github.com>
2019-10-31 12:50:50 +0400

    Added validation mechanism into Valid class

--- Cursed/Models/Data/Shared/LicenseValid.cs
+++ Cursed/Models/Data/Shared/LicenseValid.cs
@@ -1,9 +1,33 @@
-﻿using Cursed.Models.Entities;
+﻿using System;
+using Cursed.Models.Entities;
 
 namespace Cursed.Models.Data.Shared
 {
     public class LicenseValid : License
     {
         public bool IsValid { get; set; }
+
+        public LicenseValid(License license)
+        {
+            Id = license.Id;
+            ProductId = license.ProductId;
+            Date = license.Date;
+            GovermentNum = license.GovermentNum;
+            Product = license.Product;
+
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (Date > DateTime.UtcNow)
+            {
+                IsValid = true;
+            }
+            else
+            {
+                IsValid = false;
+            }
+        }
     }
 }
