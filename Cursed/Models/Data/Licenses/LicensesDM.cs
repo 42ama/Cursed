67c7889ca94b07a51221f497a3dcf431acd4ac05
42ama <45532431+42ama@users.noreply.github.com>
2019-11-01 13:16:42 +0400

    License Data and View models. For License mvc segment

---
+++ Cursed/Models/Data/Licenses/LicensesDM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cursed.Models.Data.Licenses
+{
+    public class LicensesDM
+    {
+        public int Id { get; set; }
+        public int GovermentNum { get; set; }
+        public DateTime Date { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int? ProductCAS { get; set; }
+    }
+}
---
+++ Cursed/Models/Data/Licenses/LicensesVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cursed.Models.Data.Licenses
+{
+    public class LicensesVM
+    {
+        public int Id { get; set; }
+        public int GovermentNum { get; set; }
+        public string Date { get; set; }
+        public bool IsValid { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCAS { get; set; }
+    }
+}
