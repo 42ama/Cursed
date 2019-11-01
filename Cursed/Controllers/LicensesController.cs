6bb1a5ede9908203f3a96536c7fec1b95dcffd60
42ama <45532431+42ama@users.noreply.github.com>
2019-11-01 13:18:31 +0400

    Add LicensesController Controller for License MVC

---
+++ Cursed/Controllers/LicensesController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Cursed.Models.Data.Licenses;
+using Cursed.Models.Data.Shared;
+using Cursed.Models.Context;
+using Cursed.Models.Entities;
+using Cursed.Models.Logic;
+using Cursed.Models;
+
+
+namespace Cursed.Controllers
+{
+    [Route("licenses")]
+    public class LicensesController : Controller, IControllerRESTAsync<License>
+    {
+        private readonly LicenseLogic logic;
+        public LicensesController(CursedContext db)
+        {
+            logic = new LicenseLogic(db);
+        }
+
+        [HttpGet("", Name = "LicensesAll")]
+        public async Task<IActionResult> Index()
+        {
+            var dataModel = await logic.GetAllDataModelAsync();
+            var viewModel = new List<LicensesVM>();
+            foreach (var item in dataModel)
+            {
+                viewModel.Add(new LicensesVM
+                {
+                    Id = item.Id,
+                    GovermentNum = item.GovermentNum,
+                    Date = item.Date.ToShortDateString(),
+                    ProductId = item.ProductId,
+                    ProductName = item.ProductName,
+                    ProductCAS = item.ProductCAS != null ? item.ProductCAS.Value.ToString() : "None",
+                    IsValid = LicenseValid.Validate(item.Date)
+                });
+            }
+            return View(viewModel);
+        }
+
+        [HttpGet("license", Name = "LicensesSingle")]
+        public async Task<IActionResult> SingleItem(int id)
+        {
+            var dataModel = await logic.GetSingleDataModelAsync(id);
+            var viewModel = new LicensesVM
+            {
+                Id = dataModel.Id,
+                GovermentNum = dataModel.GovermentNum,
+                Date = dataModel.Date.ToShortDateString(),
+                ProductId = dataModel.ProductId,
+                ProductName = dataModel.ProductName,
+                ProductCAS = dataModel.ProductCAS != null ? dataModel.ProductCAS.Value.ToString() : "None",
+                IsValid = LicenseValid.Validate(dataModel.Date)
+            };
+            return View(viewModel);
+        }
+
+        [HttpGet("license/edit", Name = "GetForEditLicensesSingle")]
+        public async Task<IActionResult> GetEditSingleItem(int? id)
+        {
+            var dataModel = await logic.GetSingleUpdateModelAsync(id);
+            return View(dataModel);
+        }
+
+        [HttpPost("license", Name = "LicensesSingle")]
+        public async Task<IActionResult> AddSingleItem(License model)
+        {
+            return RedirectToRoute("LicensesAll");
+        }
+
+        [HttpPut("license", Name = "LicensesSingle")]
+        public async Task<IActionResult> EditSingleItem(License model)
+        {
+            return RedirectToRoute("LicensesAll");
+        }
+
+        [HttpDelete("license", Name = "LicensesSingle")]
+        public async Task<IActionResult> DeleteSingleItem(int id)
+        {
+            return RedirectToRoute("LicensesAll");
+        }
+    }
+}
