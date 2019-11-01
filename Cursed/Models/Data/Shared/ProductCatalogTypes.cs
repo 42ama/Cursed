8e0fdc9dee3c2785ccb8280a8e90436bdab43a7b
42ama <45532431+42ama@users.noreply.github.com>
2019-11-01 11:06:59 +0400

    Edit Single Item view and minor changes to Index and SingleItem view. -New view: EditSingleItem used in preparation for Post/Put action. -EditSingleItem using string-"enum" ProductCatalogTypes, which definitely states Type prop values. -Minor tweaked form posting in Index, SingleItem

---
+++ Cursed/Models/Data/Shared/ProductCatalogTypes.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Cursed.Models.Data.Shared
+{
+    public static class ProductCatalogTypes
+    {
+        public static string Product { get { return "product"; } }
+        public static string Material { get { return "material"; } }
+    }
+}
