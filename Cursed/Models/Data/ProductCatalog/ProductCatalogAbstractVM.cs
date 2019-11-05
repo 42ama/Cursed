9a0419672ace2d5883a3af76b5cedb12e482d6d0
42ama <45532431+42ama@users.noreply.github.com>
2019-11-05 15:12:38 +0400

    Updated ProductCatalog VM family. LicenseRequired property moved to parent

--- Cursed/Models/Data/ProductCatalog/ProductCatalogAbstractVM.cs
+++ Cursed/Models/Data/ProductCatalog/ProductCatalogAbstractVM.cs
@@ -8,5 +8,6 @@ namespace Cursed.Models.Data.ProductCatalog
         public string Name { get; set; }
         public string CAS { get; set; }
         public string Type { get; set; }
+        public bool LicenseRequired { get; set; }
     }
 }
--- Cursed/Models/Data/ProductCatalog/ProductCatalogSingleVM.cs
+++ Cursed/Models/Data/ProductCatalog/ProductCatalogSingleVM.cs
@@ -6,7 +6,6 @@ namespace Cursed.Models.Data.ProductCatalog
 {
     public class ProductCatalogSingleVM : ProductCatalogAbstractVM
     {
-        public bool LicenseRequired { get; set; }
         public List<LicenseValid> Licenses { get; set; }
         public List<TitleIdContainer> Recipes { get; set; }
         public List<TitleIdContainer> Storages { get; set; }
