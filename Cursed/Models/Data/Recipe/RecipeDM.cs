a79a70484919d0e529ca5dfa235e67b62f310ac7
42ama <45532431+42ama@users.noreply.github.com>
2019-11-07 17:31:00 +0400

    Added models for Recipe section. - Added datamodel, due to #4 viewmodel separation is suspended for a while - Added RecipeProductContainer for storing information about consumed/produced products

---
+++ Cursed/Models/Data/Recipe/RecipeDM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cursed.Models.Data.Shared;
+
+namespace Cursed.Models.Data.Recipe
+{
+    public class RecipeDM
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public bool? TechApproved { get; set; }
+        public bool? GovApproved { get; set; }
+        public List<RecipeProductContainer> RecipeProducts { get; set; }
+        public int? ParentRecipe { get; set; }
+        public List<int> ChildRecipes { get; set; }
+
+    }
+}
---
+++ Cursed/Models/Data/Shared/RecipeProductContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cursed.Models.Data.Shared
+{
+    public class RecipeProductContainer
+    {
+        public int ProductId { get; set; }
+        public int? CAS { get; set; }
+        public string ProductName { get; set; }
+        public string Type { get; set; }
+        public decimal Quantity { get; set; } 
+    }
+}
