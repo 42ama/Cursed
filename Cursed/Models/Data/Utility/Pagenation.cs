c7d57702be5efe99e4a67a1d4d2f6570d4546aa8
42ama <45532431+42ama@users.noreply.github.com>
2019-11-11 13:39:14 +0400

    Updated Pagenation. Removed legacy code. Removed storing both collection and pagenatedCollection. Now contains only pagenated version.

--- Cursed/Models/Data/Utility/Pagenation.cs
+++ Cursed/Models/Data/Utility/Pagenation.cs
@@ -7,19 +7,21 @@ namespace Cursed.Models.Data.Utility
 {
     public class Pagenation<T> : PagenationAbstract
     {
-        override public int Total { get { return (int)Math.Ceiling(Collection.Count / (double)ItemsOnPage); } }
-        public List<T> Collection { get; protected set; }
+        override public int Total { get; protected set; }
         public List<T> PagenatedCollection { get; protected set; }
         
         public Pagenation(IEnumerable<T> collection, int itemsOnPage, int currentPage)
         {
-            Collection = collection.ToList();
             ItemsOnPage = itemsOnPage;
             CurrentPage = currentPage;
 
+            List<T> Collection = collection.ToList();
+
+            Total = (int)Math.Ceiling(Collection.Count / (double)ItemsOnPage);
+            
+
             if (currentPage >= 1 && currentPage <= Total)
             {
-                // ArgumentException: Offset and length were out of bounds for the array or count is greater than the number of elements from index to the end of the source collection.
                 int itemsToCopy = ItemsOnPage;
                 if ((currentPage - 1) * ItemsOnPage + ItemsOnPage > Collection.Count)
                 {
@@ -33,36 +35,4 @@ namespace Cursed.Models.Data.Utility
             }
         }
     }
-    /*public class Pagenation<T>
-    {
-        public List<T> Collection { get; private set; }
-        public List<T> PagenatedCollection { get; private set; }
-        public int ItemsOnPage { get; private set; }
-        public int CurrentPage { get; private set; }
-        public int Total { get { return (int)Math.Ceiling(Collection.Count / (double)ItemsOnPage); } }
-        public bool Next { get { return CurrentPage < Total; } }
-        public bool Prev { get { return CurrentPage > 1; } }
-
-        public Pagenation(IEnumerable<T> collection, int itemsOnPage, int currentPage)
-        {
-            Collection = collection.ToList();
-            ItemsOnPage = itemsOnPage;
-            CurrentPage = currentPage;
-
-            if (currentPage >= 1 && currentPage <= Total)
-            {
-                // ArgumentException: Offset and length were out of bounds for the array or count is greater than the number of elements from index to the end of the source collection.
-                int itemsToCopy = ItemsOnPage;
-                if ((currentPage-1)*ItemsOnPage + ItemsOnPage > Collection.Count)
-                {
-                    itemsToCopy = Collection.Count - (currentPage - 1) * ItemsOnPage;
-                }
-                PagenatedCollection = Collection.GetRange((currentPage - 1) * ItemsOnPage, itemsToCopy);
-            }
-            else
-            {
-                throw new ArgumentOutOfRangeException("currentPage", "Index of page must be in range between 0 and total count of pages");
-            }
-        }
-    }*/
 }
