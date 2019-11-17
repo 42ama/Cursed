cd3dc5d7d7464aae444c66dd8321215e7e0e5231
42ama <45532431+42ama@users.noreply.github.com>
2019-11-17 15:58:35 +0400

    Added models for Company section

---
+++ Cursed/Models/Data/Company/CompanyAllModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cursed.Models.Data.Company
+{
+    public class CompanyAllModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int StoragesCount { get; set; }
+        public int TransactionsCount { get; set; }
+    }
+}
---
+++ Cursed/Models/Data/Company/CompanySingleModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cursed.Models.Data.Utility;
+
+namespace Cursed.Models.Data.Company
+{
+    public class CompanySingleModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<TitleIdContainer> Storages { get; set; }
+        public List<TitleIdContainer> Transactions { get; set; } //title is date string
+    }
+}
