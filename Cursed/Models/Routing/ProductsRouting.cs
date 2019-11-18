ea850da7079367617bc2222c7f87562576714abc
42ama <45532431+42ama@users.noreply.github.com>
2019-11-18 14:49:05 +0400

    Added Products Routing for future section

---
+++ Cursed/Models/Routing/ProductsRouting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cursed.Models.Routing
+{
+    public class ProductsRouting
+    {
+        public const string Index = "GetProducts";
+        public const string SingleItem = "GetProduct";
+        public const string GetEditSingleItem = "GetProductForEdit";
+        public const string AddSingleItem = "AddProduct";
+        public const string EditSingleItem = "EditProduct";
+        public const string DeleteSingleItem = "DeleteProduct";
+    }
+}
