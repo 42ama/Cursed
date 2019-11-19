74f3aa276efdeacaf5110a509c0e3572e69e98c5
42ama <45532431+42ama@users.noreply.github.com>
2019-11-19 15:32:00 +0400

    Created models to new Facilities section

---
+++ Cursed/Models/Data/Facilities/FacilitiesAbstractModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Cursed.Models.Data.Facilities
+{
+    public abstract class FacilitiesAbstractModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+    }
+}
---
+++ Cursed/Models/Data/Facilities/FacilityModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Cursed.Models.Entities;
+using Cursed.Models.Data.Shared;
+
+namespace Cursed.Models.Data.Facilities
+{
+    public class FacilityModel : FacilitiesAbstractModel
+    {
+        public List<FacilitiesProductContainer> Products { get; set; }
+    }
+}
