644e092a03c4a14d90c03b4b930d9d808391dd84
42ama <45532431+42ama@users.noreply.github.com>
2019-11-19 15:33:08 +0400

    Added controller and logic model for new Facilities section

---
+++ Cursed/Controllers/FacilitiesController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Cursed.Models.Data.Facilities;
+using Cursed.Models.Data.Shared;
+using Cursed.Models.Context;
+using Cursed.Models.Entities;
+using Cursed.Models.Logic;
+using Cursed.Models.Routing;
+using Cursed.Models;
+using Cursed.Models.Data.Utility;
+
+namespace Cursed.Controllers
+{
+    [Route("facilities")]
+    public class FacilitiesController : Controller
+    {
+        private readonly FacilitiesLogic logic;
+
+        public FacilitiesController(CursedContext db)
+        {
+            logic = new FacilitiesLogic(db);
+        }
+        [HttpGet("", Name = FacilitiesRouting.Index)]
+        public async Task<IActionResult> Index(int currentPage = 1, int itemsOnPage = 20)
+        {
+            var model = await logic.GetAllDataModelAsync();
+            var pagenationModel = new Pagenation<FacilitiesModel>(model, itemsOnPage, currentPage);
+
+            return View(pagenationModel);
+        }
+
+        [HttpGet("facility", Name = FacilitiesRouting.SingleItem)]
+        public async Task<IActionResult> SingleItem(string key)
+        {
+            int id = Int32.Parse(key);
+            var model = await logic.GetSingleDataModelAsync(id);
+            return View(model);
+        }
+
+        [HttpGet("facility/edit", Name = FacilitiesRouting.GetEditSingleItem)]
+        public async Task<IActionResult> GetEditSingleItem(string key)
+        {
+            if (key != null)
+            {
+                int id = Int32.Parse(key);
+                ViewData["SaveRoute"] = FacilitiesRouting.EditSingleItem;
+                var model = await logic.GetSingleUpdateModelAsync(id);
+                return View("EditSingleItem", model);
+            }
+            else
+            {
+                ViewData["SaveRoute"] = FacilitiesRouting.AddSingleItem;
+                return View("EditSingleItem");
+            }
+        }
+
+        [HttpPost("facility/add", Name = FacilitiesRouting.AddSingleItem)]
+        public async Task<IActionResult> AddSingleItem(Facility model)
+        {
+            await logic.AddDataModelAsync(model);
+            return RedirectToRoute(FacilitiesRouting.Index);
+        }
+
+        [HttpPost("facility/edit", Name = FacilitiesRouting.EditSingleItem)]
+        public async Task<IActionResult> EditSingleItem(Facility model)
+        {
+            await logic.UpdateDataModelAsync(model);
+            return RedirectToRoute(FacilitiesRouting.Index);
+        }
+
+        [HttpPost("facility/delete", Name = FacilitiesRouting.DeleteSingleItem)]
+        public async Task<IActionResult> DeleteSingleItem(string key)
+        {
+            int id = Int32.Parse(key);
+            await logic.RemoveDataModelAsync(id);
+            return RedirectToRoute(FacilitiesRouting.Index);
+        }
+    }
+}
---
+++ Cursed/Models/Logic/FacilitiesLogic.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Cursed.Models.Context;
+using Cursed.Models.Data.Facilities;
+using Cursed.Models.Entities;
+using Cursed.Models.Data.Shared;
+using Cursed.Models.Data.Utility;
+
+namespace Cursed.Models.Logic
+{
+    public class FacilitiesLogic : IRESTAsync<FacilitiesModel, FacilityModel, Facility>
+    {
+        private readonly CursedContext db;
+        public FacilitiesLogic(CursedContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IEnumerable<FacilitiesModel>> GetAllDataModelAsync()
+        {
+            var facilities = await db.Facility.ToListAsync();
+            var query = from f in facilities
+                        join q in (from f in facilities
+                                   join tp in db.TechProcess on f.Id equals tp.FacilityId into recipes
+                                   from r in recipes.DefaultIfEmpty()
+                                   group r by f.Id) on f.Id equals q.Key into techprocesses
+                        from tab in techprocesses.DefaultIfEmpty()
+                        select new FacilitiesModel
+                        {
+                            Id = f.Id,
+                            Latitude = f.Latitude,
+                            Longitude = f.Longitude,
+                            Name = f.Name,
+                            TechProcesses = tab.ToList()
+                        };
+
+            return query;
+        }
+
+        public async Task<FacilityModel> GetSingleDataModelAsync(object key)
+        {
+            
+            var facilities = await db.Facility.ToListAsync();
+            var licensesList = await db.License.ToListAsync();
+
+            var queryInner = from f in facilities
+                             where f.Id == (int)key
+                             join tp in db.TechProcess on f.Id equals tp.FacilityId into techprocesses
+                             from tps in techprocesses.DefaultIfEmpty()
+                             join r in db.Recipe on tps.RecipeId equals r.Id into recipes
+                             from rs in recipes.DefaultIfEmpty()
+                             join rpc in db.RecipeProductChanges on rs.Id equals rpc.RecipeId into recipeproducts
+                             from rpcs in recipeproducts.DefaultIfEmpty()
+                             join pc in db.ProductCatalog on rpcs.ProductId equals pc.Id into products
+                             from pcs in products.DefaultIfEmpty()
+                             group new FacilitiesProductContainer()
+                             {
+                                 FacilityId = f.Id,
+                                 RecipeId = tps.RecipeId,
+                                 RecipeEfficiency = tps.DayEffiency,
+                                 RecipeGovApprov = rs.GovermentApproval ?? false,
+                                 RecipeTechnoApprov = rs.TechApproval ?? false,
+                                 ProductId = rpcs.ProductId,
+                                 ProductType = rpcs.Type,
+                                 Quantity = rpcs.Quantity,
+                                 ProductName = pcs.Name,
+                                 LicenseRequired = pcs.LicenseRequired ?? false
+                             } by f.Id;
+
+            // for each product connect license from previously loaded list and set IsValid property,
+            // baseed on validation
+            foreach (var group in queryInner)
+            {
+                foreach (var item in group)
+                {
+                    if(item.LicenseRequired)
+                    {
+                        item.IsValid = false;
+                        var licenses = licensesList.Where(i => i.ProductId == item.ProductId);
+                        foreach (var license in licenses)
+                        {
+                            if (LicenseValid.Validate(license))
+                            {
+                                item.IsValid = true;
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        item.IsValid = true;
+                    }
+                }
+            }
+
+            var queryOuter = from f in facilities
+                        where f.Id == (int)key
+                        join qi in queryInner on f.Id equals qi.Key
+                        select new FacilityModel
+                        {
+                            Id = f.Id,
+                            Latitude = f.Latitude,
+                            Longitude = f.Longitude,
+                            Name = f.Name,
+                            Products = qi.ToList()
+                        };
+
+            return queryOuter.Single();
+        }
+
+
+        public async Task<Facility> GetSingleUpdateModelAsync(object key)
+        {
+            return await db.Facility.SingleOrDefaultAsync(i => i.Id == (int)key);
+        }
+
+        public async Task AddDataModelAsync(Facility dataModel)
+        {
+            dataModel.Id = default;
+            db.Add(dataModel);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task UpdateDataModelAsync(Facility updatedDataModel)
+        {
+            var currentModel = await db.Facility.FirstOrDefaultAsync(i => i.Id == updatedDataModel.Id);
+            db.Entry(currentModel).CurrentValues.SetValues(updatedDataModel);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task RemoveDataModelAsync(object key)
+        {
+            var entity = await db.Facility.FindAsync((int)key);
+
+            db.Facility.Remove(entity);
+
+            await db.SaveChangesAsync();
+        }
+    }
+}
