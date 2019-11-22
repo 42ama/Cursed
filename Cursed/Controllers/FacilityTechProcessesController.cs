8473afa002dace2c0c3356f578387878236281a9
42ama <45532431+42ama@users.noreply.github.com>
2019-11-22 10:49:22 +0400

    New section FacilityTechProcesses. Provides means to edit tech processes.

---
+++ Cursed/Controllers/FacilityTechProcessesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Cursed.Models.Data.FacilityTechProcesses;
+using Cursed.Models.Data.Shared;
+using Cursed.Models.Context;
+using Cursed.Models.Entities;
+using Cursed.Models.Logic;
+using Cursed.Models.Routing;
+using Cursed.Models.Interfaces.ControllerCRUD;
+using Cursed.Models.Data.Utility;
+
+namespace Cursed.Controllers
+{
+    [Route("facilities/tech-processes")]
+    public class FacilityTechProcessesController : Controller
+    {
+        private readonly FacilityTechProcessesLogic logic;
+        public FacilityTechProcessesController(CursedContext db)
+        {
+            logic = new FacilityTechProcessesLogic(db);
+        }
+
+        [HttpGet("", Name = FacilityTechProcessesRouting.Index)]
+        public async Task<IActionResult> Index(string key, int currentPage = 1, int itemsOnPage = 20)
+        {
+            int facilityId = Int32.Parse(key);
+            ViewData["FacilityId"] = facilityId;
+            var model = await logic.GetAllDataModelAsync(facilityId);
+            var pagenationModel = new Pagenation<FacilityTechProcessesDataModel>(model, itemsOnPage, currentPage);
+            return View(pagenationModel);
+        }
+
+        [HttpPost("add", Name = FacilityTechProcessesRouting.AddSingleItem)]
+        public async Task<IActionResult> AddSingleItem(TechProcess model)
+        {
+            await logic.AddDataModelAsync(model);
+            return RedirectToRoute(FacilityTechProcessesRouting.Index, new { key = model.FacilityId });
+        }
+
+        [HttpPost("edit", Name = FacilityTechProcessesRouting.EditSingleItem)]
+        public async Task<IActionResult> EditSingleItem(TechProcess model)
+        {
+            await logic.UpdateDataModelAsync(model);
+            return RedirectToRoute(FacilityTechProcessesRouting.Index, new { key = model.FacilityId });
+        }
+
+        [HttpPost("delete", Name = FacilityTechProcessesRouting.DeleteSingleItem)]
+        public async Task<IActionResult> DeleteSingleItem(TechProcess model)
+        {
+            await logic.RemoveDataModelAsync(model);
+            return RedirectToRoute(FacilityTechProcessesRouting.Index, new { key = model.FacilityId });
+        }
+    }
+}
---
+++ Cursed/Models/Routing/FacilityTechProcessesRouting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cursed.Models.Routing
+{
+    public class FacilityTechProcessesRouting
+    {
+        public const string Index = "GetFacilityTechProcesses";
+        public const string AddSingleItem = "AddFacilityTechProcess";
+        public const string EditSingleItem = "EditFacilityTechProcess";
+        public const string DeleteSingleItem = "DeleteFacilityTechProcess";
+    }
+}
