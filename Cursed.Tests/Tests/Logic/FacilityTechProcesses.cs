3af16f3f626544ec8ebd774a7c540a21e765264d
42ama <45532431+42ama@users.noreply.github.com>
2019-11-27 11:44:01 +0400

    Added tests for FacilityTechProcesses logic model

---
+++ Cursed.Tests/Tests/Logic/FacilityTechProcesses.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Cursed.Models.Logic;
+using Cursed.Models.Entities;
+
+namespace Cursed.Tests.Tests.Logic
+{
+    public class FacilityTechProcesses : IClassFixture<TestsFixture>
+    {
+        private readonly TestsFixture fixture;
+        private readonly FacilityTechProcessesLogic logic;
+
+        public FacilityTechProcesses(TestsFixture fixture)
+        {
+            this.fixture = fixture;
+            logic = new FacilityTechProcessesLogic(fixture.db);
+        }
+
+        [Fact]
+        public async void AddDataModelAsync()
+        {
+            // arrange
+            var entityModel = new TechProcess
+            {
+                FacilityId = 44440,
+                RecipeId = 44440,
+                DayEffiency = (decimal)12.3456
+            };
+
+            // act
+            await logic.AddDataModelAsync(entityModel);
+
+            // assert
+            var testModel = await fixture.db.TechProcess.FirstOrDefaultAsync(i => i.FacilityId == entityModel.FacilityId && i.RecipeId == entityModel.RecipeId);
+            Assert.Equal(entityModel, testModel);
+
+            // dispose
+            fixture.db.TechProcess.Remove(testModel);
+            await fixture.db.SaveChangesAsync();
+        }
+
+        [Fact]
+        public async void RemoveDataModelAsync()
+        {
+            // arrange
+            var entityModel = new TechProcess
+            {
+                FacilityId = 44440,
+                RecipeId = 44440,
+                DayEffiency = (decimal)12.3456
+            };
+            fixture.db.Add(entityModel);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            await logic.RemoveDataModelAsync(entityModel);
+
+            // assert
+            var testModel = await fixture.db.TechProcess.FirstOrDefaultAsync(i => i.FacilityId == entityModel.FacilityId && i.RecipeId == entityModel.RecipeId);
+            Assert.Null(testModel);
+        }
+
+        [Fact]
+        public async void UpdateDataModelAsync()
+        {
+            // arrange
+            var entityModel = new TechProcess
+            {
+                FacilityId = 44440,
+                RecipeId = 44440,
+                DayEffiency = (decimal)12.3456
+            };
+            fixture.db.Add(entityModel);
+            await fixture.db.SaveChangesAsync();
+
+            var updatedModel = new TechProcess
+            {
+                FacilityId = 44440,
+                RecipeId = 44440,
+                DayEffiency = (decimal)78.9012
+            };
+
+            // act
+            await logic.UpdateDataModelAsync(updatedModel);
+
+            // assert
+            var testModel = await fixture.db.TechProcess.FirstOrDefaultAsync(i => i.FacilityId == entityModel.FacilityId && i.RecipeId == entityModel.RecipeId);
+            Assert.Equal(updatedModel.DayEffiency, testModel.DayEffiency);
+
+            // dispose
+            fixture.db.TechProcess.Remove(testModel);
+            await fixture.db.SaveChangesAsync();
+        }
+    }
+}
