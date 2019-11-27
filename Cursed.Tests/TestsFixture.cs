09a7cdde31611c7b5d8f94a41fa97bb98be45462
42ama <45532431+42ama@users.noreply.github.com>
2019-11-27 10:59:56 +0400

    Added packages, added TestFixture which provide database to tests.

---
+++ Cursed.Tests/TestsFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Cursed.Models.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cursed.Tests
+{
+    public class TestsFixture : IDisposable
+    {
+        public readonly CursedContext db;
+
+        public TestsFixture()
+        {
+            
+            var options = new DbContextOptionsBuilder<CursedContext>()
+                .UseInMemoryDatabase(databaseName: "CursedTestingDB")
+                .Options;
+
+            db = new CursedContext(options);
+        }
+
+        public void Dispose()
+        {
+            db.Database.EnsureDeleted();
+        }
+    }
+}
