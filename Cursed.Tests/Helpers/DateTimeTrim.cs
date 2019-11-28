1985db66d7243f25dbcd9ab16fbc5e2daf6c67c8
42ama <45532431+42ama@users.noreply.github.com>
2019-11-28 09:58:07 +0400

    Added Helpers namespace and Helper static class. Added DateTimeTrim to Helper.

---
+++ Cursed.Tests/Helpers/DateTimeTrim.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cursed.Tests.Helpers
+{
+    public static partial class Helper
+    {
+        public static DateTime Trim(this DateTime date, long roundTicks)
+        {
+            return new DateTime(date.Ticks - date.Ticks % roundTicks, date.Kind);
+        }
+    }
+}
