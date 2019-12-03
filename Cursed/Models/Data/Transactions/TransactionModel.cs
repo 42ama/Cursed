5adf111a2075dae295d5c4ff576336db569e49d6
42ama <45532431+42ama@users.noreply.github.com>
2019-12-03 13:10:00 +0400

    Added models for new Transaction section

---
+++ Cursed/Models/Data/Transactions/TransactionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cursed.Models.Entities;
+
+namespace Cursed.Models.Data.Transactions
+{
+    public class TransactionModel : TransactionsAbstractModel
+    {
+        public List<Operation> Operations { get; set; }
+    }
+}
