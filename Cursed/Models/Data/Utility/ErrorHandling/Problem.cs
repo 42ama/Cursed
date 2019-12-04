b53433aef531b554ca49ebb658f7969a1b975e93
42ama <45532431+42ama@users.noreply.github.com>
2019-12-04 14:09:45 +0400

    Added Problem and Status Message to handle erros in logic

---
+++ Cursed/Models/Data/Utility/ErrorHandling/Problem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cursed.Models.Data.Utility.ErrorHandling
+{
+    public struct Problem
+    {
+        public string Entity { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Cursed/Models/Data/Utility/ErrorHandling/StatusMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cursed.Models.Data.Utility.ErrorHandling
+{
+    public abstract class AbstractStatusMessage
+    {
+        public bool IsCompleted
+        {
+            get
+            {
+                if(Problems.Count > 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        public string Entity { get; set; }
+        public List<Problem> Problems { get; set; }
+    }
+
+    public class StatusMessage : AbstractStatusMessage
+    {
+    }
+
+    public class StatusMessage<T> : AbstractStatusMessage
+    {
+        public T RetrunValue { get; set; }
+    }
+}
