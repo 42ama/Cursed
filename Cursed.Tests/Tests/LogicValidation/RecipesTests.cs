3ca755f7d21ae02001b66f314f81fa82a31c255d
42ama <45532431+42ama@users.noreply.github.com>
2019-12-09 12:39:20 +0400

    #35 Added tests for RecipesLogicValidation. Added RecipesLogicValidation.

---
+++ Cursed.Tests/Tests/LogicValidation/RecipesTests.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Cursed.Models.LogicValidation;
+using Cursed.Models.Entities;
+using Cursed.Models.Data.Companies;
+using Cursed.Models.Data.Shared;
+using Cursed.Tests.Extensions;
+
+namespace Cursed.Tests.Tests.LogicValidation
+{
+    [Collection("Tests collection")]
+    public class RecipesTests : IDisposable
+    {
+        private readonly TestsFixture fixture;
+        private readonly RecipesLogicValidation logicValidation;
+
+        public RecipesTests(TestsFixture fixture)
+        {
+            this.fixture = fixture;
+            logicValidation = new RecipesLogicValidation(fixture.db);
+        }
+
+        public async void Dispose()
+        {
+            await TestsFixture.ClearDatabase(fixture.db);
+        }
+
+        private Recipe GetRecipe()
+        {
+            return new Recipe
+            {
+                Id = 44440,
+                Content = "Test content",
+                GovermentApproval = true,
+                TechApproval = false
+            };
+        }
+
+        private RecipeInheritance GetRecipeInheritance()
+        {
+            return new RecipeInheritance
+            {
+                ParentId = 44440,
+                ChildId = 44441
+            };
+        }
+
+        private RecipeProductChanges GetRecipeProductChanges()
+        {
+            return new RecipeProductChanges
+            {
+                ProductId = 44440,
+                RecipeId = 44440,
+                Quantity = (decimal)12.3456,
+                Type = ProductCatalogTypes.Product
+            };
+        }
+
+        private TechProcess GetTechProcess()
+        {
+            return new TechProcess
+            {
+                FacilityId = 44440,
+                RecipeId = 44440,
+                DayEffiency = (decimal)12.3456
+            };
+        }
+
+        [Fact]
+        public async void CheckRemoveRecipe_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var recipe = GetRecipe();
+            fixture.db.Add(recipe);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync(recipe.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckRemoveRecipe_FromInitializedDbTableWithRelatedEntities_ErrorHandlerIsCompletedFalseHaveSpecificProblems()
+        {
+            // arrange
+            var recipe = GetRecipe();
+            var recipeInheritance = GetRecipeInheritance();
+            var techProcess = GetTechProcess();
+            var productChanges = GetRecipeProductChanges();
+            fixture.db.Add(recipe);
+            fixture.db.Add(recipeInheritance);
+            fixture.db.Add(techProcess);
+            fixture.db.Add(productChanges);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync(recipe.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+            Assert.Contains(statusMessage.Problems, problem =>
+                problem.Entity == "Recipe." && (int)problem.EntityKey == recipeInheritance.ChildId);
+            Assert.Contains(statusMessage.Problems, problem =>
+                problem.Entity == "Tech process." && 
+                ((ValueTuple<int, int>)problem.EntityKey).Item1 == techProcess.RecipeId &&
+                ((ValueTuple<int, int>)problem.EntityKey).Item2 == techProcess.FacilityId);
+            Assert.Contains(statusMessage.Problems, problem =>
+                problem.Entity == "Recipe product changes." &&
+                ((ValueTuple<int, int>)problem.EntityKey).Item1 == productChanges.RecipeId &&
+                ((ValueTuple<int, int>)problem.EntityKey).Item2 == productChanges.ProductId);
+        }
+
+        [Fact]
+        public async void CheckGetRecipe_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var recipe = GetRecipe();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleDataModelAsync(recipe.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetRecipe_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var recipe = GetRecipe();
+            fixture.db.Add(recipe);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleDataModelAsync(recipe.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetRecipeForUpdate_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var recipe = GetRecipe();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUpdateModelAsync(recipe.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetRecipeForUpdate_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var recipe = GetRecipe();
+            fixture.db.Add(recipe);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUpdateModelAsync(recipe.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateRecipe_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var recipe = GetRecipe();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(recipe.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateRecipe_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var recipe = GetRecipe();
+            fixture.db.Add(recipe);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(recipe.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+    }
+}
---
+++ Cursed/Models/LogicValidation/RecipesLogicValidation.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Cursed.Models.Context;
+using Cursed.Models.Data.Companies;
+using Cursed.Models.Entities;
+using Cursed.Models.Interfaces.LogicCRUD;
+using Cursed.Models.Data.Utility;
+using Cursed.Models.Data.Utility.ErrorHandling;
+
+namespace Cursed.Models.LogicValidation
+{
+    public class RecipesLogicValidation
+    {
+        private readonly CursedContext db;
+        private readonly AbstractErrorHandlerFactory errorHandlerFactory;
+
+        public RecipesLogicValidation(CursedContext db)
+        {
+            this.db = db;
+            errorHandlerFactory = new StatusMessageFactory();
+        }
+
+        public async Task<AbstractErrorHandler> CheckGetSingleDataModelAsync(object key)
+        {
+            return await CheckExists(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckGetSingleUpdateModelAsync(object key)
+        {
+            return await CheckExists(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckUpdateDataModelAsync(object key)
+        {
+            return await CheckExists(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckRemoveDataModelAsync(object key)
+        {
+            var statusMessage = await CheckExists(key);
+
+            if (!statusMessage.IsCompleted)
+            {
+                return statusMessage;
+            }
+
+            // check related entities
+            var childs = db.RecipeInheritance.Where(i => i.ParentId == (int)key);
+            var techProcesses = db.TechProcess.Where(i => i.RecipeId == (int)key);
+            var productsChanges = db.RecipeProductChanges.Where(i => i.RecipeId == (int)key);
+
+            if (childs.Any())
+            {
+                foreach (var child in childs)
+                {
+                    statusMessage.Problems.Add(new Problem
+                    {
+                        Entity = "Recipe.",
+                        EntityKey = child.ChildId,
+                        Message = "Recipe have childs."
+                    });
+                }
+            }
+            if (techProcesses.Any())
+            {
+                foreach (var techProcess in techProcesses)
+                {
+                    statusMessage.Problems.Add(new Problem
+                    {
+                        Entity = "Tech process.",
+                        EntityKey = (techProcess.RecipeId, techProcess.FacilityId),
+                        Message = "Recipe have related tech process."
+                    });
+                }
+            }
+            if (productsChanges.Any())
+            {
+                foreach (var productChanges in productsChanges)
+                {
+                    statusMessage.Problems.Add(new Problem
+                    {
+                        Entity = "Recipe product changes.",
+                        EntityKey = (productChanges.RecipeId, productChanges.ProductId),
+                        Message = "Recipe have related recipe product changes."
+                    });
+                }
+            }
+
+            return statusMessage;
+        }
+        private async Task<AbstractErrorHandler> CheckExists(object key)
+        {
+            var statusMessage = errorHandlerFactory.NewErrorHandler("Recipe.", key);
+
+            if (await db.Recipe.FirstOrDefaultAsync(i => i.Id == (int)key) == null)
+            {
+                statusMessage.AddProblem(new Problem
+                {
+                    Entity = "Recipe.",
+                    EntityKey = key,
+                    Message = "No recipe with such key found."
+                });
+            }
+
+            return statusMessage;
+        }
+    }
+}
