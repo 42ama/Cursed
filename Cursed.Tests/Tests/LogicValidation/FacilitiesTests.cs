094e8f9a62e7cfbcce9d5a4dfa597d0a2240f95c
42ama <45532431+42ama@users.noreply.github.com>
2019-12-09 11:20:10 +0400

    Added tests for FacilitiesLogicValidation. Added FacilitiesLogicValidation.

---
+++ Cursed.Tests/Tests/LogicValidation/FacilitiesTests.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Cursed.Models.LogicValidation;
+using Cursed.Models.Entities;
+using Cursed.Models.Data.Facilities;
+using Cursed.Models.Data.Shared;
+using Cursed.Tests.Extensions;
+using Cursed.Tests.Stubs;
+
+namespace Cursed.Tests.Tests.LogicValidation
+{
+    [Collection("Tests collection")]
+    public class FacilitiesTests : IDisposable
+    {
+        private readonly TestsFixture fixture;
+        private readonly FacilitiesLogicValidation logicValidation;
+
+        public FacilitiesTests(TestsFixture fixture)
+        {
+            this.fixture = fixture;
+            logicValidation = new FacilitiesLogicValidation(fixture.db);
+        }
+
+        public async void Dispose()
+        {
+            await TestsFixture.ClearDatabase(fixture.db);
+        }
+
+        private Facility GetFacility()
+        {
+            return new Facility
+            {
+                Id = 44440,
+                Name = "Test facility #1",
+                Latitude = (decimal)12.3456,
+                Longitude = (decimal)78.9012
+            };
+        }
+        private IEnumerable<TechProcess> GetTechProcesses()
+        {
+            return new TechProcess[]
+            {
+                new TechProcess
+                {
+                    FacilityId = 44440,
+                    RecipeId = 44440,
+                    DayEffiency = (decimal)12.3456
+                },
+                new TechProcess
+                {
+                    FacilityId = 44440,
+                    RecipeId = 44441,
+                    DayEffiency = (decimal)12.3456
+                }
+            };
+        }
+
+        [Fact]
+        public async void CheckRemoveFacility_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var facility = GetFacility();
+            fixture.db.Add(facility);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync(facility.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckRemoveFacility_FromInitializedDbTableWithRelatedEntities_ErrorHandlerIsCompletedFalseHaveSpecificProblems()
+        {
+            // arrange
+            var facility = GetFacility();
+            var techProcesses = GetTechProcesses();
+            fixture.db.Add(facility);
+            fixture.db.AddRange(techProcesses);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync(facility.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+            foreach (var techProcess in techProcesses)
+            {
+                Assert.Contains(statusMessage.Problems, problem =>
+                problem.Entity == "Tech process." && 
+                ((ValueTuple<int, int>)problem.EntityKey).Item1 == techProcess.FacilityId && 
+                ((ValueTuple<int, int>)problem.EntityKey).Item2 == techProcess.RecipeId);
+            }
+        }
+
+        [Fact]
+        public async void CheckGetFacility_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var facility = GetFacility();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleDataModelAsync(facility.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetFacility_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var facility = GetFacility();
+            fixture.db.Add(facility);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleDataModelAsync(facility.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetFacilityForUpdate_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var facility = GetFacility();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUpdateModelAsync(facility.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetFacilityForUpdate_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var facility = GetFacility();
+            fixture.db.Add(facility);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUpdateModelAsync(facility.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateFacility_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var facility = GetFacility();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(facility.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateFacility_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var facility = GetFacility();
+            fixture.db.Add(facility);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(facility.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+    }
+}
---
+++ Cursed/Models/LogicValidation/FacilitiesLogicValidation.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Cursed.Models.Context;
+using Cursed.Models.Data.Companies;
+using Cursed.Models.Entities;
+using Cursed.Models.Interfaces.LogicCRUD;
+using Cursed.Models.Data.Utility;
+using Cursed.Models.Data.Utility.ErrorHandling;
+
+namespace Cursed.Models.LogicValidation
+{
+    public class FacilitiesLogicValidation
+    {
+        private readonly CursedContext db;
+        private readonly AbstractErrorHandlerFactory errorHandlerFactory;
+
+        public FacilitiesLogicValidation(CursedContext db)
+        {
+            this.db = db;
+            errorHandlerFactory = new StatusMessageFactory();
+        }
+
+        public async Task<AbstractErrorHandler> CheckGetSingleDataModelAsync(object key)
+        {
+            return await CheckExists(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckGetSingleUpdateModelAsync(object key)
+        {
+            return await CheckExists(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckUpdateDataModelAsync(object key)
+        {
+            return await CheckExists(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckRemoveDataModelAsync(object key)
+        {
+            var statusMessage = await CheckExists(key);
+
+            if (!statusMessage.IsCompleted)
+            {
+                return statusMessage;
+            }
+
+            // check related entities
+            var techProcesses = db.TechProcess.Where(i => i.FacilityId == (int)key);
+
+            if (techProcesses.Any())
+            {
+                foreach (var techProcess in techProcesses)
+                {
+                    statusMessage.Problems.Add(new Problem
+                    {
+                        Entity = "Tech process.",
+                        EntityKey = (facilityId: techProcess.FacilityId, recipeId: techProcess.RecipeId),
+                        Message = "Facility have related techprocesses."
+                    });
+                }
+            }
+
+            return statusMessage;
+        }
+        private async Task<AbstractErrorHandler> CheckExists(object key)
+        {
+            var statusMessage = errorHandlerFactory.NewErrorHandler("Facility.", key);
+
+            if (await db.Facility.FirstOrDefaultAsync(i => i.Id == (int)key) == null)
+            {
+                statusMessage.AddProblem(new Problem
+                {
+                    Entity = "Facility.",
+                    EntityKey = key,
+                    Message = "No facility with such key found."
+                });
+            }
+
+            return statusMessage;
+        }
+    }
+}
