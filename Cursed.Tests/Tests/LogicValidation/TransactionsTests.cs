15c5c85ea5d2f936cc4fcb32d578c6301c6412c0
42ama <45532431+42ama@users.noreply.github.com>
2019-12-09 13:21:33 +0400

    #35 Added tests for TransactionsLogicValidation. Added TransactionsLogicValidation.

---
+++ Cursed.Tests/Tests/LogicValidation/TransactionsTests.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Cursed.Models.LogicValidation;
+using Cursed.Models.Entities;
+using Cursed.Models.Data.Companies;
+using Cursed.Models.Data.Shared;
+using Cursed.Tests.Stubs;
+
+namespace Cursed.Tests.Tests.LogicValidation
+{
+    [Collection("Tests collection")]
+    public class TransactionsTests : IDisposable
+    {
+        private readonly TestsFixture fixture;
+        private readonly TransactionsLogicValidation logicValidation;
+
+        public TransactionsTests(TestsFixture fixture)
+        {
+            this.fixture = fixture;
+            logicValidation = new TransactionsLogicValidation(fixture.db, new OperationValidationStub());
+        }
+
+        public async void Dispose()
+        {
+            await TestsFixture.ClearDatabase(fixture.db);
+        }
+
+        private TransactionBatch GetTransaction()
+        {
+            return new TransactionBatch
+            {
+                Id = 44440,
+                Date = DateTime.UtcNow,
+                CompanyId = 44440,
+                Type = TransactionTypes.Income,
+                IsOpen = true,
+                Comment = "Why chicken cross the road?"
+            };
+        }
+
+        [Fact]
+        public async void CheckRemoveTransaction_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var transaction = GetTransaction();
+            fixture.db.Add(transaction);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync(transaction.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckRemoveTransaction_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var transaction = GetTransaction();
+            
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync(transaction.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckRemoveTransaction_FromInitializedDbTableButClosed_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var transaction = GetTransaction();
+            transaction.IsOpen = false;
+            fixture.db.Add(transaction);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync(transaction.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateTransaction_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var transaction = GetTransaction();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(transaction.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateTransaction_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var transaction = GetTransaction();
+            fixture.db.Add(transaction);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(transaction.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateTransaction_FromInitializedDbTableButClosed_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var transaction = GetTransaction();
+            transaction.IsOpen = false;
+            fixture.db.Add(transaction);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(transaction.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetTransaction_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var transaction = GetTransaction();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleDataModelAsync(transaction.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetTransaction_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var transaction = GetTransaction();
+            fixture.db.Add(transaction);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleDataModelAsync(transaction.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetTransactionForUpdate_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var transaction = GetTransaction();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUpdateModelAsync(transaction.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetTransactionForUpdate_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var transaction = GetTransaction();
+            fixture.db.Add(transaction);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUpdateModelAsync(transaction.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+    }
+}
---
+++ Cursed/Models/LogicValidation/TransactionsLogicValidation.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Cursed.Models.Context;
+using Cursed.Models.Data.Companies;
+using Cursed.Models.Entities;
+using Cursed.Models.Services;
+using Cursed.Models.Data.Utility;
+using Cursed.Models.Data.Utility.ErrorHandling;
+
+namespace Cursed.Models.LogicValidation
+{
+    public class TransactionsLogicValidation
+    {
+        private readonly CursedContext db;
+        private readonly IOperationValidation operationValidation;
+        private readonly AbstractErrorHandlerFactory errorHandlerFactory;
+
+        public TransactionsLogicValidation(CursedContext db, IOperationValidation operationValidation)
+        {
+            this.db = db;
+            this.operationValidation = operationValidation;
+            errorHandlerFactory = new StatusMessageFactory();
+            
+        }
+
+        public async Task<AbstractErrorHandler> CheckGetSingleDataModelAsync(object key)
+        {
+            return await CheckExists(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckGetSingleUpdateModelAsync(object key)
+        {
+            return await CheckExists(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckUpdateDataModelAsync(object key)
+        {
+            var statusMessage = await CheckExists(key);
+
+            if (!statusMessage.IsCompleted)
+            {
+                return statusMessage;
+            }
+
+            return await CheckClosed(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckRemoveDataModelAsync(object key)
+        {
+            var statusMessage = await CheckExists(key);
+
+            if (!statusMessage.IsCompleted)
+            {
+                return statusMessage;
+            }
+
+            return await CheckClosed(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckCloseTransactionAsync(object key)
+        {
+            var statusMessage = await CheckExists(key);
+
+            if (!statusMessage.IsCompleted)
+            {
+                return statusMessage;
+            }
+
+            // operation validation
+            var transaction = await db.TransactionBatch.SingleAsync(i => i.Id == (int)key);
+            foreach (var operation in transaction.Operation)
+            {
+                var operationMessage = await operationValidation.IsValidAsync(operation);
+                if (!operationMessage.IsCompleted)
+                {
+                    foreach (var problem in operationMessage?.Problems)
+                    {
+                        statusMessage.Problems.Add(new Problem
+                        {
+                            Entity = operationMessage.Entity + problem.Entity,
+                            Message = problem.Entity
+                        });
+                    }
+                }
+            }
+
+            return await CheckClosed(key);
+        }
+
+        private async Task<AbstractErrorHandler> CheckExists(object key)
+        {
+            var statusMessage = errorHandlerFactory.NewErrorHandler("Transaction.", key);
+            var transaction = await db.TransactionBatch.FirstOrDefaultAsync(i => i.Id == (int)key);
+            if (transaction == null)
+            {
+                statusMessage.AddProblem(new Problem
+                {
+                    Entity = "Transaction.",
+                    EntityKey = key,
+                    Message = "No transaction with such key found."
+                });
+            }
+            
+            return statusMessage;
+        }
+
+        private async Task<AbstractErrorHandler> CheckClosed(object key)
+        {
+            var statusMessage = errorHandlerFactory.NewErrorHandler("Transaction.", key);
+            var transaction = await db.TransactionBatch.FirstOrDefaultAsync(i => i.Id == (int)key);
+
+            if (!transaction.IsOpen)
+            {
+                statusMessage.Problems.Add(new Problem
+                {
+                    Entity = "Transaction open status.",
+                    Message = "Can't change transaction, when it closed."
+                });
+            }
+
+            return statusMessage;
+        }
+    }
+}
