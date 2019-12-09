26f91a652610bfe6db014bf696b228b52bc2b59d
42ama <45532431+42ama@users.noreply.github.com>
2019-12-09 12:24:37 +0400

    #35 Added tests for RecipeProductsChangesLogicValidation. Added RecipeProductsChangesLogicValidation.

---
+++ Cursed.Tests/Tests/LogicValidation/RecipeProductsTests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Cursed.Models.LogicValidation;
+using Cursed.Models.Entities;
+using Cursed.Models.Data.Facilities;
+using Cursed.Models.Data.Shared;
+using Cursed.Tests.Extensions;
+using Cursed.Tests.Stubs;
+
+namespace Cursed.Tests.Tests.LogicValidation
+{
+    [Collection("Tests collection")]
+    public class RecipeProductsTests : IDisposable
+    {
+        private readonly TestsFixture fixture;
+        private readonly RecipeProductsLogicValidation logicValidation;
+
+        public RecipeProductsTests(TestsFixture fixture)
+        {
+            this.fixture = fixture;
+            logicValidation = new RecipeProductsLogicValidation(fixture.db);
+        }
+
+        public async void Dispose()
+        {
+            await TestsFixture.ClearDatabase(fixture.db);
+        }
+        private RecipeProductChanges GetRecipeProductChanges()
+        {
+            return new RecipeProductChanges
+            {
+                ProductId = 44440,
+                RecipeId = 44440,
+                Quantity = (decimal)12.3456,
+                Type = ProductCatalogTypes.Product
+            };
+        }
+
+        private Recipe GetRecipe()
+        {
+            return new Recipe
+            {
+                Id = 44440,
+                Content = "Test content",
+                GovermentApproval = true,
+                TechApproval = false
+            };
+        }
+
+        private ProductCatalog GetProductCatalog()
+        {
+            return new ProductCatalog
+            {
+                Id = 44440,
+                Cas = 4040404,
+                LicenseRequired = true,
+                Name = "Testotin",
+                Type = ProductCatalogTypes.Product
+            };
+        }
+        
+        [Fact]
+        public async void CheckRemoveRecipeProduct_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var recipeProductChanges = GetRecipeProductChanges();
+            fixture.db.Add(recipeProductChanges);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync((recipeProductChanges.RecipeId, recipeProductChanges.ProductId));
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckRemoveRecipeProduct_FromInitializedDbTableWithRelatedEntities_ErrorHandlerIsCompletedFalseHaveSpecificProblems()
+        {
+            // arrange
+            var recipeProductChanges = GetRecipeProductChanges();
+            var recipe = GetRecipe();
+            var productCatalog = GetProductCatalog();
+            fixture.db.Add(recipeProductChanges);
+            fixture.db.Add(recipe);
+            fixture.db.Add(productCatalog);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync((recipeProductChanges.RecipeId, recipeProductChanges.ProductId));
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+            Assert.Contains(statusMessage.Problems, problem =>
+            problem.Entity == "Product in catalog." && (int)problem.EntityKey == productCatalog.Id);
+            Assert.Contains(statusMessage.Problems, problem =>
+            problem.Entity == "Recipe." && (int)problem.EntityKey == recipe.Id);
+
+        }
+
+        [Fact]
+        public async void CheckUpdateRecipeProduct_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var recipeProductChanges = GetRecipeProductChanges();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync((recipeProductChanges.RecipeId, recipeProductChanges.ProductId));
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateRecipeProduct_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var recipeProductChanges = GetRecipeProductChanges();
+            fixture.db.Add(recipeProductChanges);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync((recipeProductChanges.RecipeId, recipeProductChanges.ProductId));
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+    }
+}
---
+++ Cursed/Models/LogicValidation/RecipeProductsLogicValidation.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Cursed.Models.Context;
+using Cursed.Models.Data.Companies;
+using Cursed.Models.Entities;
+using Cursed.Models.Interfaces.LogicCRUD;
+using Cursed.Models.Data.Utility;
+using Cursed.Models.Data.Utility.ErrorHandling;
+
+namespace Cursed.Models.LogicValidation
+{
+    public class RecipeProductsLogicValidation
+    {
+        private readonly CursedContext db;
+        private readonly AbstractErrorHandlerFactory errorHandlerFactory;
+
+        public RecipeProductsLogicValidation(CursedContext db)
+        {
+            this.db = db;
+            errorHandlerFactory = new StatusMessageFactory();
+        }
+
+        public async Task<AbstractErrorHandler> CheckUpdateDataModelAsync(object key)
+        {
+            return await CheckExists(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckRemoveDataModelAsync(object key)
+        {
+            var statusMessage = await CheckExists(key);
+
+            if (!statusMessage.IsCompleted)
+            {
+                return statusMessage;
+            }
+
+            // check related entities
+            var recipes = db.Recipe.Where(i => i.Id == ((ValueTuple<int, int>)key).Item1);
+            var productsCatalog = db.ProductCatalog.Where(i => i.Id == ((ValueTuple<int, int>)key).Item2);
+            
+
+            if (productsCatalog.Any())
+            {
+                foreach (var productCatalog in productsCatalog)
+                {
+                    statusMessage.Problems.Add(new Problem
+                    {
+                        Entity = "Product in catalog.",
+                        EntityKey = productCatalog.Id,
+                        Message = "Recipe products changes have related product in catalog."
+                    });
+                }
+            }
+
+            if (recipes.Any())
+            {
+                foreach (var recipe in recipes)
+                {
+                    statusMessage.Problems.Add(new Problem
+                    {
+                        Entity = "Recipe.",
+                        EntityKey = recipe.Id,
+                        Message = "Recipe products changes have related recipe."
+                    });
+                }
+            }
+
+            return statusMessage;
+        }
+        private async Task<AbstractErrorHandler> CheckExists(object key)
+        {
+            var statusMessage = errorHandlerFactory.NewErrorHandler("Tech process.", key);
+
+            if (await db.RecipeProductChanges.FirstOrDefaultAsync(i => i.RecipeId == ((ValueTuple<int, int>)key).Item1 &&
+            i.ProductId == ((ValueTuple<int, int>)key).Item2) == null)
+            {
+                statusMessage.AddProblem(new Problem
+                {
+                    Entity = "Recipe products changes.",
+                    EntityKey = key,
+                    Message = "No recipe products changes with such key's found."
+                });
+            }
+
+            return statusMessage;
+        }
+    }
+}
