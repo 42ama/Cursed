1d3ba4aa2e3248e91336b36360085897b25c7383
42ama <45532431+42ama@users.noreply.github.com>
2019-12-09 12:16:00 +0400

    #35 Added tests for ProductsCatalogLogicValidation. Added ProductsCatalogLogicValidation.

---
+++ Cursed.Tests/Tests/LogicValidation/ProductsCatalogTests.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Cursed.Models.LogicValidation;
+using Cursed.Models.Entities;
+using Cursed.Models.Data.Companies;
+using Cursed.Models.Data.Shared;
+using Cursed.Tests.Extensions;
+
+namespace Cursed.Tests.Tests.LogicValidation
+{
+    [Collection("Tests collection")]
+    public class ProductsCatalogTests : IDisposable
+    {
+        private readonly TestsFixture fixture;
+        private readonly ProductsCatalogLogicValidation logicValidation;
+
+        public ProductsCatalogTests(TestsFixture fixture)
+        {
+            this.fixture = fixture;
+            logicValidation = new ProductsCatalogLogicValidation(fixture.db);
+        }
+
+        public async void Dispose()
+        {
+            await TestsFixture.ClearDatabase(fixture.db);
+        }
+
+        private ProductCatalog GetProductCatalog()
+        {
+            return new ProductCatalog
+            {
+                Id = 44440,
+                Cas = 4040404,
+                LicenseRequired = true,
+                Name = "Testotin",
+                Type = ProductCatalogTypes.Product
+            };
+        }
+
+        private Product GetProduct()
+        {
+            return new Product
+            {
+                Id = 44440,
+                Uid = 44440,
+                StorageId = 44440,
+                Price = 17,
+                Quantity = 12,
+                QuantityUnit = "mg."
+            };
+        }
+
+        private License GetLicense()
+        {
+            return new License
+            {
+                Id = 44440,
+                ProductId = 44440,
+                Date = DateTime.UtcNow,
+                GovermentNum = 4040404
+            };
+        }
+
+        private RecipeProductChanges GetRecipeProductChanges()
+        {
+            return new RecipeProductChanges
+            {
+                ProductId = 44440,
+                RecipeId = 44440,
+                Quantity = (decimal)12.3456,
+                Type = ProductCatalogTypes.Product
+            };
+        }
+
+        [Fact]
+        public async void CheckRemoveProductCatalog_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var productCatalog = GetProductCatalog();
+            fixture.db.Add(productCatalog);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync(productCatalog.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckRemoveProductCatalog_FromInitializedDbTableWithRelatedEntities_ErrorHandlerIsCompletedFalseHaveSpecificProblems()
+        {
+            // arrange
+            var productCatalog = GetProductCatalog();
+            var product = GetProduct();
+            var recipeProductChanges = GetRecipeProductChanges();
+            var license = GetLicense();
+            fixture.db.Add(productCatalog);
+            fixture.db.Add(product);
+            fixture.db.Add(recipeProductChanges);
+            fixture.db.Add(license);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync(productCatalog.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+            Assert.Contains(statusMessage.Problems, problem =>
+            problem.Entity == "Product." && (int)problem.EntityKey == product.Id);
+            Assert.Contains(statusMessage.Problems, problem =>
+            problem.Entity == "Recipe product changes." && 
+            ((ValueTuple<int, int>)problem.EntityKey).Item1 == recipeProductChanges.ProductId &&
+            ((ValueTuple<int, int>)problem.EntityKey).Item2 == recipeProductChanges.RecipeId);
+            Assert.Contains(statusMessage.Problems, problem =>
+            problem.Entity == "License." && (int)problem.EntityKey == license.Id);
+        }
+
+        [Fact]
+        public async void CheckGetProductCatalog_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var productCatalog = GetProductCatalog();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleDataModelAsync(productCatalog.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetProductCatalog_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var productCatalog = GetProductCatalog();
+            fixture.db.Add(productCatalog);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleDataModelAsync(productCatalog.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetProductCatalogForUpdate_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var productCatalog = GetProductCatalog();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUpdateModelAsync(productCatalog.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetProductCatalogForUpdate_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var productCatalog = GetProductCatalog();
+            fixture.db.Add(productCatalog);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUpdateModelAsync(productCatalog.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateProductCatalog_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var productCatalog = GetProductCatalog();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(productCatalog.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateProductCatalog_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var productCatalog = GetProductCatalog();
+            fixture.db.Add(productCatalog);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(productCatalog.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+    }
+}
---
+++ Cursed/Models/LogicValidation/ProductsCatalogLogicValidation.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Cursed.Models.Context;
+using Cursed.Models.Data.Companies;
+using Cursed.Models.Entities;
+using Cursed.Models.Interfaces.LogicCRUD;
+using Cursed.Models.Data.Utility;
+using Cursed.Models.Data.Utility.ErrorHandling;
+
+namespace Cursed.Models.LogicValidation
+{
+    public class ProductsCatalogLogicValidation
+    {
+        private readonly CursedContext db;
+        private readonly AbstractErrorHandlerFactory errorHandlerFactory;
+
+        public ProductsCatalogLogicValidation(CursedContext db)
+        {
+            this.db = db;
+            errorHandlerFactory = new StatusMessageFactory();
+        }
+
+        public async Task<AbstractErrorHandler> CheckGetSingleDataModelAsync(object key)
+        {
+            return await CheckExists(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckGetSingleUpdateModelAsync(object key)
+        {
+            return await CheckExists(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckUpdateDataModelAsync(object key)
+        {
+            return await CheckExists(key);
+        }
+
+        public async Task<AbstractErrorHandler> CheckRemoveDataModelAsync(object key)
+        {
+            var statusMessage = await CheckExists(key);
+
+            if (!statusMessage.IsCompleted)
+            {
+                return statusMessage;
+            }
+
+            // check related entities
+            var recipeProductsChanges = db.RecipeProductChanges.Where(i => i.ProductId == (int)key);
+            var products = db.Product.Where(i => i.Uid == (int)key);
+            var licenses = db.License.Where(i => i.ProductId == (int)key);
+
+            if (recipeProductsChanges.Any())
+            {
+                foreach (var recipeProductChanges in recipeProductsChanges)
+                {
+                    statusMessage.Problems.Add(new Problem
+                    {
+                        Entity = "Recipe product changes.",
+                        EntityKey = (productId: recipeProductChanges.ProductId, recipeId: recipeProductChanges.RecipeId),
+                        Message = "Product in catalog have related recipe product changes."
+                    });
+                }
+            }
+            if (products.Any())
+            {
+                foreach (var product in products)
+                {
+                    statusMessage.Problems.Add(new Problem
+                    {
+                        Entity = "Product.",
+                        EntityKey = product.Id,
+                        Message = "Product in catalog have related product in storage."
+                    });
+                }
+            }
+            if (licenses.Any())
+            {
+                foreach (var license in licenses)
+                {
+                    statusMessage.Problems.Add(new Problem
+                    {
+                        Entity = "License.",
+                        EntityKey = license.Id,
+                        Message = "Product in catalog have related license."
+                    });
+                }
+            }
+
+            return statusMessage;
+        }
+        private async Task<AbstractErrorHandler> CheckExists(object key)
+        {
+            var statusMessage = errorHandlerFactory.NewErrorHandler("Products in catalog.", key);
+
+            if (await db.ProductCatalog.FirstOrDefaultAsync(i => i.Id == (int)key) == null)
+            {
+                statusMessage.AddProblem(new Problem
+                {
+                    Entity = "Products in catalog.",
+                    EntityKey = key,
+                    Message = "No products in catalog with such key found."
+                });
+            }
+
+            return statusMessage;
+        }
+    }
+}
