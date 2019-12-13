1997b6d19a15f2859346bc20aafdcaf0ffea4a83
42ama <45532431+42ama@users.noreply.github.com>
2019-12-13 12:41:21 +0400

    Added tests for Opertaion section

---
+++ Cursed.Tests/Tests/Logic/OperationsTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Cursed.Models.Logic;
+using Cursed.Models.Entities;
+using Cursed.Models.Data.Shared;
+
+namespace Cursed.Tests.Tests.Logic
+{
+    [Collection("Tests collection")]
+    public class OperationsTests : IDisposable
+    {
+        private readonly TestsFixture fixture;
+        private readonly OperationsLogic logic;
+
+        public OperationsTests(TestsFixture fixture)
+        {
+            this.fixture = fixture;
+            logic = new OperationsLogic(fixture.db);
+        }
+
+        public async void Dispose()
+        {
+            await TestsFixture.ClearDatabase(fixture.db);
+        }
+
+        private Operation GetOperation()
+        {
+            return new Operation
+            {
+                Id = 44440,
+                ProductId = 44440,
+                Price = 12,
+                Quantity = 14,
+                StorageFromId = 44440,
+                StorageToId = 44441,
+                TransactionId = 44440
+            };
+        }
+
+        [Fact]
+        public async void AddOperation_ToEmptyDbTable_AddedOperationEqualExpectedOperation()
+        {
+            // arrange
+            var expected = GetOperation();
+
+            // act
+            await logic.AddDataModelAsync(expected);
+
+            // assert
+            var actual = await fixture.db.Operation.FirstOrDefaultAsync(i => i.Id == expected.Id);
+            Assert.Equal(expected.Id, actual.Id);
+            Assert.Equal(expected.Price, actual.Price);
+            Assert.Equal(expected.ProductId, actual.ProductId);
+            Assert.Equal(expected.Quantity, actual.Quantity);
+            Assert.Equal(expected.StorageFromId, actual.StorageFromId);
+            Assert.Equal(expected.StorageToId, actual.StorageToId);
+            Assert.Equal(expected.TransactionId, actual.TransactionId);
+        }
+
+        [Fact]
+        public async void RemoveOperation_FromInitializedDbTable_RemovedOperationNotFoundInDb()
+        {
+            // arrange
+            var operation = GetOperation();
+            fixture.db.Add(operation);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            await logic.RemoveDataModelAsync(operation.Id);
+
+            // assert
+            var actual = await fixture.db.Operation.FirstOrDefaultAsync(i => i.Id == operation.Id);
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public async void UpdateOperation_AtInitializedDbTable_UpdatedOperationEqualExpectedOperation()
+        {
+            // arrange
+            var operation = GetOperation();
+            fixture.db.Add(operation);
+            await fixture.db.SaveChangesAsync();
+
+            var expected = new Operation
+            {
+                Id = 44440,
+                ProductId = 44440,
+                Price = 220,
+                Quantity = 220,
+                StorageFromId = 44440,
+                StorageToId = 44441,
+                TransactionId = 44440
+            };
+
+            // act
+            await logic.UpdateDataModelAsync(expected);
+
+            // assert
+            var actual = await fixture.db.Operation.FirstOrDefaultAsync(i => i.Id == expected.Id);
+            Assert.Equal(expected.Id, actual.Id);
+            Assert.Equal(expected.Price, actual.Price);
+            Assert.Equal(expected.ProductId, actual.ProductId);
+            Assert.Equal(expected.Quantity, actual.Quantity);
+            Assert.Equal(expected.StorageFromId, actual.StorageFromId);
+            Assert.Equal(expected.StorageToId, actual.StorageToId);
+            Assert.Equal(expected.TransactionId, actual.TransactionId);
+        }
+
+        [Fact]
+        public async void GetOperation_FromInitializedDbTable_LogicOperationEqualExpectedOperation()
+        {
+            // arrange
+            var expected = GetOperation();
+
+            fixture.db.Add(expected);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var actual = await logic.GetSingleUpdateModelAsync(expected.Id);
+
+            // assert
+            Assert.Equal(expected.Id, actual.Id);
+            Assert.Equal(expected.Price, actual.Price);
+            Assert.Equal(expected.ProductId, actual.ProductId);
+            Assert.Equal(expected.Quantity, actual.Quantity);
+            Assert.Equal(expected.StorageFromId, actual.StorageFromId);
+            Assert.Equal(expected.StorageToId, actual.StorageToId);
+            Assert.Equal(expected.TransactionId, actual.TransactionId);
+        }
+    }
+}
---
+++ Cursed.Tests/Tests/LogicValidation/OperationsTests.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Cursed.Models.LogicValidation;
+using Cursed.Models.Entities;
+using Cursed.Models.Data.Companies;
+using Cursed.Models.Data.Shared;
+using Cursed.Tests.Extensions;
+using Cursed.Models.Data.Utility.ErrorHandling;
+
+namespace Cursed.Tests.Tests.LogicValidation
+{
+    [Collection("Tests collection")]
+    public class OperationsTests : IDisposable
+    {
+        private readonly TestsFixture fixture;
+        private readonly OperationsLogicValidation logicValidation;
+
+        public OperationsTests(TestsFixture fixture)
+        {
+            this.fixture = fixture;
+            logicValidation = new OperationsLogicValidation(fixture.db, new StatusMessageFactory());
+        }
+
+        public async void Dispose()
+        {
+            await TestsFixture.ClearDatabase(fixture.db);
+        }
+
+        private Operation GetOperation()
+        {
+            return new Operation
+            {
+                Id = 44440,
+                ProductId = 44440,
+                Price = 12,
+                Quantity = 14,
+                StorageFromId = 44440,
+                StorageToId = 44441,
+                TransactionId = 44440
+            };
+        }
+
+        private TransactionBatch GetOpenTransaction()
+        {
+            return new TransactionBatch
+            {
+                Id = 44440,
+                CompanyId = 44440,
+                Date = DateTime.UtcNow,
+                IsOpen = true,
+                Type = TransactionTypes.Income,
+                Comment = "This is open transaction."
+            };
+        }
+        private TransactionBatch GetClosedTransaction()
+        {
+            return new TransactionBatch
+            {
+                Id = 44440,
+                CompanyId = 44440,
+                Date = DateTime.UtcNow,
+                IsOpen = false,
+                Type = TransactionTypes.Income,
+                Comment = "This is closed transaction."
+            };
+        }
+
+        [Fact]
+        public async void CheckRemoveOperation_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var operation = GetOperation();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync(operation.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckRemoveOperation_FromInitializedDbTableOpenTransaction_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var operation = GetOperation();
+            var transaction = GetOpenTransaction();
+            fixture.db.Add(operation);
+            fixture.db.Add(transaction);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync(operation.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckRemoveOperation_FromInitializedDbTableClosedTransaction_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var operation = GetOperation();
+            var transaction = GetClosedTransaction();
+            fixture.db.Add(operation);
+            fixture.db.Add(transaction);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync(operation.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetOperationForUpdate_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var operation = GetOperation();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUpdateModelAsync(operation.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetOperationForUpdate_FromInitializedDbTableClosedTransaction_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var operation = GetOperation();
+            var transaction = GetClosedTransaction();
+            fixture.db.Add(operation);
+            fixture.db.Add(transaction);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUpdateModelAsync(operation.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetOperationForUpdate_FromInitializedDbTableOpenTransaction_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var operation = GetOperation();
+            var transaction = GetOpenTransaction();
+            fixture.db.Add(operation);
+            fixture.db.Add(transaction);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUpdateModelAsync(operation.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateOperation_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var operation = GetOperation();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(operation.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateOperation_FromInitializedDbTableClosedTransaction_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var operation = GetOperation();
+            var transaction = GetClosedTransaction();
+            fixture.db.Add(operation);
+            fixture.db.Add(transaction);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(operation.Id);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateOperation_FromInitializedDbTableOpenTransaction_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var operation = GetOperation();
+            var transaction = GetOpenTransaction();
+            fixture.db.Add(operation);
+            fixture.db.Add(transaction);
+            await fixture.db.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(operation.Id);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+    }
+}
