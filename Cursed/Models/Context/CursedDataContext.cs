16b32b267d5027ea26cb5b0fe60210ab9df39da9
42ama <45532431+42ama@users.noreply.github.com>
2020-01-08 10:44:03 +0400

    Updated CursedDataContext, cause of separation authentication database and data database.

--- Cursed/Models/Context/CursedDataContext.cs
+++ Cursed/Models/Context/CursedDataContext.cs
@@ -20,19 +20,15 @@ namespace Cursed.Models.Context
         public virtual DbSet<Facility> Facility { get; set; }
         public virtual DbSet<License> License { get; set; }
         public virtual DbSet<Operation> Operation { get; set; }
-        public virtual DbSet<Policy> Policy { get; set; }
         public virtual DbSet<Product> Product { get; set; }
         public virtual DbSet<ProductCatalog> ProductCatalog { get; set; }
         public virtual DbSet<Recipe> Recipe { get; set; }
         public virtual DbSet<RecipeInheritance> RecipeInheritance { get; set; }
         public virtual DbSet<RecipeProductChanges> RecipeProductChanges { get; set; }
-        public virtual DbSet<Role> Role { get; set; }
-        public virtual DbSet<RoleHavePolicy> RoleHavePolicy { get; set; }
         public virtual DbSet<Storage> Storage { get; set; }
         public virtual DbSet<TechProcess> TechProcess { get; set; }
         public virtual DbSet<TransactionBatch> TransactionBatch { get; set; }
-        public virtual DbSet<UserAuth> UserAuth { get; set; }
-        public virtual DbSet<UserData> UserData { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -94,13 +90,6 @@ namespace Cursed.Models.Context
                     .HasForeignKey(d => d.TransactionId)
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("FK_Operation_Transaction_Id");
-            });
-
-            modelBuilder.Entity<Policy>(entity =>
-            {
-                entity.Property(e => e.Name)
-                    .IsRequired()
-                    .HasMaxLength(50);
             });
 
             modelBuilder.Entity<Product>(entity =>
@@ -191,33 +180,7 @@ namespace Cursed.Models.Context
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("FK_RecipeProductChanges_Recipe_Id");
             });
-
-            modelBuilder.Entity<Role>(entity =>
-            {
-                entity.Property(e => e.Name)
-                    .IsRequired()
-                    .HasMaxLength(50);
-            });
-
-            modelBuilder.Entity<RoleHavePolicy>(entity =>
-            {
-                //entity.HasNoKey();
-                entity.HasKey(e => new { e.RoleId, e.PolicyId })
-                    .HasName("CK_RoleHavePolicy_RoleId_PolicyId");
-
-                entity.HasOne(d => d.Policy)
-                    .WithMany(p => p.RoleHavePolicy)
-                    .HasForeignKey(d => d.PolicyId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_RoleHavePolicy_Policy_Id");
-
-                entity.HasOne(d => d.Role)
-                    .WithMany(p => p.RoleHavePolicy)
-                    .HasForeignKey(d => d.RoleId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_RoleHavePolicy_Role_Id");
-            });
-
+            
             modelBuilder.Entity<Storage>(entity =>
             {
                 entity.Property(e => e.Latitude).HasColumnType("decimal(8, 6)");
@@ -272,41 +235,6 @@ namespace Cursed.Models.Context
                     .HasForeignKey(d => d.CompanyId)
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("FK_Operation_Company_Id");
-            });
-
-            modelBuilder.Entity<UserAuth>(entity =>
-            {
-                entity.HasKey(e => e.Login)
-                    .HasName("PK_UserAuth_Login_Clustered");
-
-                entity.Property(e => e.Login)
-                    .HasMaxLength(39)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.PasswordHash)
-                    .IsRequired()
-                    .HasMaxLength(48)
-                    .IsUnicode(false);
-            });
-
-            modelBuilder.Entity<UserData>(entity =>
-            {
-                entity.HasKey(e => e.Login)
-                    .HasName("PK_UserData_Login_Clustered");
-
-                entity.Property(e => e.Login)
-                    .HasMaxLength(39)
-                    .IsUnicode(false);
-
-                entity.HasOne(d => d.LoginNavigation)
-                    .WithOne(p => p.UserData)
-                    .HasForeignKey<UserData>(d => d.Login)
-                    .OnDelete(DeleteBehavior.ClientSetNull);
-
-                entity.HasOne(d => d.Role)
-                    .WithMany(p => p.UserData)
-                    .HasForeignKey(d => d.RoleId)
-                    .OnDelete(DeleteBehavior.ClientSetNull);
             });
 
             OnModelCreatingPartial(modelBuilder);
