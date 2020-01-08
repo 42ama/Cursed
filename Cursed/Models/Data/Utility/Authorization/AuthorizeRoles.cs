d2074c00c2a8d9df931de94400cda73eb194e91f
42ama <45532431+42ama@users.noreply.github.com>
2020-01-08 09:40:20 +0400

    Added compile time ready class with user roles - AuthorizeRoles. And custom version of Authorize attribute - AuthorizeRoles, utilizing more readable accepting Roles as attributes.

---
+++ Cursed/Models/Data/Utility/Authorization/AuthorizeRoles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cursed.Models.Data.Utility.Authorization
+{
+    public class AuthorizeRoles
+    {
+        public const string Administrator = "ADMIN";
+        public const string Technologist = "TECHNOLOGIST";
+        public const string SeniorTechnologist = "SENIORTECHNOLOGIST";
+        public const string GovermentAgent = "GOVERMENTAGENT";
+        public const string Manager = "MANAGER";
+    }
+    
+}
