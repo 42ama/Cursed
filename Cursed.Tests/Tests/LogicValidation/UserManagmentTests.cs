654b44216ff1fa31e040cc2e0b41c6192ea3124f
42ama <45532431+42ama@users.noreply.github.com>
2020-01-11 12:02:32 +0400

    Added Logic Validation for UserManagment section with related Tests. Added Routing for UserManagment section.

---
+++ Cursed.Tests/Tests/LogicValidation/UserManagmentTests.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Cursed.Models.LogicValidation;
+using Cursed.Models.Entities;
+using Cursed.Models.Data.Companies;
+using Cursed.Models.Data.Utility;
+using Cursed.Tests.Extensions;
+using Cursed.Models.Data.Utility.ErrorHandling;
+using Cursed.Models.Services;
+using Cursed.Models.Entities.Authentication;
+
+namespace Cursed.Tests.Tests.LogicValidation
+{
+    [Collection("Tests collection")]
+    public class UserManagmentTests : IDisposable
+    {
+        private readonly TestsFixture fixture;
+        private readonly UserManagmentLogicValidation logicValidation;
+
+        public UserManagmentTests(TestsFixture fixture)
+        {
+            this.fixture = fixture;
+            logicValidation = new UserManagmentLogicValidation(fixture.dbAuth, new StatusMessageFactory());
+        }
+
+        public async void Dispose()
+        {
+            await TestsFixture.ClearDatabase(fixture.dbAuth);
+        }
+
+        private UserData GetUserData()
+        {
+            return new UserData
+            {
+                Login = "admin",
+                RoleName = "admin"
+            };
+        }
+
+        private UserAuth GetUserAuth()
+        {
+            return new UserAuth
+            {
+                Login = "admin",
+                PasswordHash = "hash"
+            };
+        }
+
+        [Fact]
+        public async void CheckRemoveUser_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var userData = GetUserData();
+            var userAuth = GetUserAuth();
+            fixture.dbAuth.Add(userData);
+            fixture.dbAuth.Add(userAuth);
+            await fixture.dbAuth.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckRemoveDataModelAsync(userData.Login);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetUser_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var userLogin = "admin";
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleDataModelAsync(userLogin);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetUser_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var userData = GetUserData();
+            var userAuth = GetUserAuth();
+            fixture.dbAuth.Add(userData);
+            fixture.dbAuth.Add(userAuth);
+            await fixture.dbAuth.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleDataModelAsync(userData.Login);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetUserDataForUpdate_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var userLogin = "admin";
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUserDataUpdateModelAsync(userLogin);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetUserDataForUpdate_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var userData = GetUserData();
+            var userAuth = GetUserAuth();
+            fixture.dbAuth.Add(userData);
+            fixture.dbAuth.Add(userAuth);
+            await fixture.dbAuth.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUserDataUpdateModelAsync(userData.Login);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetUserAuthForUpdate_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var userLogin = "admin";
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUserAuthUpdateModelAsync(userLogin);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckGetUserAuthForUpdate_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var userData = GetUserData();
+            var userAuth = GetUserAuth();
+            fixture.dbAuth.Add(userData);
+            fixture.dbAuth.Add(userAuth);
+            await fixture.dbAuth.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckGetSingleUserAuthUpdateModelAsync(userData.Login);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateUser_FromEmptyDbTable_ErrorHandlerIsCompletedFalse()
+        {
+            // arrange
+            var userLogin = "admin";
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(userLogin);
+
+            // assert
+            Assert.False(statusMessage.IsCompleted);
+        }
+
+        [Fact]
+        public async void CheckUpdateUser_FromInitializedDbTable_ErrorHandlerIsCompletedTrue()
+        {
+            // arrange
+            var userData = GetUserData();
+            var userAuth = GetUserAuth();
+            fixture.dbAuth.Add(userData);
+            fixture.dbAuth.Add(userAuth);
+            await fixture.dbAuth.SaveChangesAsync();
+
+            // act
+            var statusMessage = await logicValidation.CheckUpdateDataModelAsync(userData.Login);
+
+            // assert
+            Assert.True(statusMessage.IsCompleted);
+        }
+
+    }
+}
---
+++ Cursed/Models/LogicValidation/UserManagmentLogicValidation.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Cursed.Models.Context;
+using Cursed.Models.Data.Companies;
+using Cursed.Models.Entities;
+using Cursed.Models.Interfaces.LogicCRUD;
+using Cursed.Models.Data.Utility;
+using Cursed.Models.Data.Utility.ErrorHandling;
+using Cursed.Models.Routing;
+using Cursed.Models.Services;
+
+
+namespace Cursed.Models.LogicValidation
+{
+    public class UserManagmentLogicValidation
+    {
+        private readonly CursedAuthenticationContext db;
+        private readonly IErrorHandlerFactory errorHandlerFactory;
+
+        public UserManagmentLogicValidation(CursedAuthenticationContext db, IErrorHandlerFactory errorHandlerFactory)
+        {
+            this.db = db;
+            this.errorHandlerFactory = errorHandlerFactory;
+        }
+        // add checks for user auth
+        public async Task<IErrorHandler> CheckGetSingleDataModelAsync(object key)
+        {
+            return await CheckUserExists(key);
+        }
+
+        public async Task<IErrorHandler> CheckGetSingleUserDataUpdateModelAsync(object key)
+        {
+            return await CheckUserExists(key);
+        }
+
+        public async Task<IErrorHandler> CheckGetSingleUserAuthUpdateModelAsync(object key)
+        {
+            return await CheckUserExists(key);
+        }
+
+        public async Task<IErrorHandler> CheckUpdateDataModelAsync(object key)
+        {
+            return await CheckUserExists(key);
+        }
+
+        public async Task<IErrorHandler> CheckRemoveDataModelAsync(object key)
+        {
+            return await CheckUserExists(key);
+        }
+        private async Task<IErrorHandler> CheckUserExists(object key)
+        {
+            var statusMessage = errorHandlerFactory.NewErrorHandler(new Problem
+            {
+                Entity = "User.",
+                EntityKey = (string)key,
+                RedirectRoute = UserManagmentRouting.SingleItem
+            });
+
+            if (await db.UserData.FirstOrDefaultAsync(i => i.Login == (string)key) == null)
+            {
+                statusMessage.AddProblem(new Problem
+                {
+                    Entity = "User (data).",
+                    EntityKey = (string)key,
+                    Message = "User (data) with this is not found. Probably user just doesn't exist.",
+                    RedirectRoute = UserManagmentRouting.Index,
+                    UseKeyWithRoute = false
+                });
+            }
+
+            if (await db.UserAuth.FirstOrDefaultAsync(i => i.Login == (string)key) == null)
+            {
+                statusMessage.AddProblem(new Problem
+                {
+                    Entity = "User (auth).",
+                    EntityKey = (string)key,
+                    Message = "User (auth) with this is not found. Probably somewhere error occused, check DB for reference.",
+                    RedirectRoute = UserManagmentRouting.Index,
+                    UseKeyWithRoute = false
+                });
+            }
+
+            return statusMessage;
+        }
+    }
+}
