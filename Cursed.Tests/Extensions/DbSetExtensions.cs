a3e845e2efa90ca891fc73e2154bec910a40ea76
42ama <45532431+42ama@users.noreply.github.com>
2020-01-18 22:16:13 +0400

    Added commentary to DbSetExtenstions

--- Cursed.Tests/Extensions/DbSetExtensions.cs
+++ Cursed.Tests/Extensions/DbSetExtensions.cs
@@ -1,13 +1,18 @@
 ﻿using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
 namespace Cursed.Tests.Extensions
 {
+    /// <summary>
+    /// Extenstion to Db Set. Clears db set completly.
+    /// </summary>
     public static class DbSetExtensions
     {
-        // used to clear tables in dbset
+        /// <summary>
+        /// Clears db set completly.
+        /// </summary>
+        /// <typeparam name="T">Type of entities in db set</typeparam>
+        /// <param name="dbSet">Db set to be cleared</param>
         public static async Task ClearIfAny<T>(this DbSet<T> dbSet) where T : class
         {
             if(await dbSet.AnyAsync())
