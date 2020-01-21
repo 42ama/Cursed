a3f71b93dcb498c1ea30d5f5e1aa3255cc0609a8
42ama <45532431+42ama@users.noreply.github.com>
2020-01-21 12:02:14 +0400

    Close #62. Fixed bug.

--- Cursed/Models/Business/Logic/FacilitiesLogic.cs
+++ Cursed/Models/Business/Logic/FacilitiesLogic.cs
@@ -57,77 +57,96 @@ namespace Cursed.Models.Logic
         /// <returns>Single facility, which found by <c>key</c>. Contains more information than Facility entity.</returns>
         public async Task<FacilityModel> GetSingleDataModelAsync(object key)
         {
+            IEnumerable<FacilityModel> queryOuter;
             // gather facilities, to have unblocking call when grouping
             var facilities = await db.Facility.ToListAsync();
             // gather licenses, to have validation information on each license
             var licensesList = await db.License.ToListAsync();
 
-            // gather basic data required for display facility
-            var queryInner = (from f in facilities
-                             where f.Id == (int)key
-                             join tp in db.TechProcess on f.Id equals tp.FacilityId into techprocesses
-                             from tps in techprocesses.DefaultIfEmpty()
-                             join r in db.Recipe on tps.RecipeId equals r.Id into recipes
-                             from rs in recipes.DefaultIfEmpty()
-                             join rpc in db.RecipeProductChanges on rs.Id equals rpc.RecipeId into recipeproducts
-                             from rpcs in recipeproducts.DefaultIfEmpty()
-                             join pc in db.ProductCatalog on rpcs.ProductId equals pc.Id into products
-                             from pcs in products.DefaultIfEmpty()
-                             group new FacilitiesProductContainer()
-                             {
-                                 FacilityId = f.Id,
-                                 RecipeId = tps.RecipeId,
-                                 RecipeEfficiency = tps.DayEfficiency,
-                                 RecipeGovApprov = rs.GovermentApproval ?? false,
-                                 RecipeTechnoApprov = rs.TechApproval ?? false,
-                                 ProductId = rpcs.ProductId,
-                                 ProductType = rpcs.Type,
-                                 Quantity = rpcs.Quantity,
-                                 ProductName = pcs.Name,
-                                 LicenseRequired = pcs.LicenseRequired ?? false
-                             } by f.Id).ToList();
-
-            // for each product connect license from previously loaded list and 
-            // based on validation set IsValid property
-            foreach (var group in queryInner)
+            
+            if (await db.TechProcess.Where(i => i.FacilityId == (int)key).AnyAsync())
             {
-                foreach (var item in group)
+                // gather basic data required for display facility
+                var queryInner = (from f in facilities
+                                  where f.Id == (int)key
+                                  join tp in db.TechProcess on f.Id equals tp.FacilityId into techprocesses
+                                  from tps in techprocesses.DefaultIfEmpty()
+                                  join r in db.Recipe on tps.RecipeId equals r.Id into recipes
+                                  from rs in recipes.DefaultIfEmpty()
+                                  join rpc in db.RecipeProductChanges on rs.Id equals rpc.RecipeId into recipeproducts
+                                  from rpcs in recipeproducts.DefaultIfEmpty()
+                                  join pc in db.ProductCatalog on rpcs.ProductId equals pc.Id into products
+                                  from pcs in products.DefaultIfEmpty()
+                                  group new FacilitiesProductContainer()
+                                  {
+                                      FacilityId = f.Id,
+                                      RecipeId = tps.RecipeId,
+                                      RecipeEfficiency = tps.DayEfficiency,
+                                      RecipeGovApprov = rs.GovermentApproval ?? false,
+                                      RecipeTechnoApprov = rs.TechApproval ?? false,
+                                      ProductId = rpcs.ProductId,
+                                      ProductType = rpcs.Type,
+                                      Quantity = rpcs.Quantity,
+                                      ProductName = pcs.Name,
+                                      LicenseRequired = pcs.LicenseRequired ?? false
+                                  } by f.Id).ToList();
+
+                // for each product connect license from previously loaded list and 
+                // based on validation set IsValid property
+                foreach (var group in queryInner)
                 {
-                    if(item.LicenseRequired)
+                    foreach (var item in group)
                     {
-                        // set default to false
-                        item.IsValid = false;
-                        var licenses = licensesList.Where(i => i.ProductId == item.ProductId);
-                        // if valid license for current proudct if found, set IsValid to true
-                        foreach (var license in licenses)
+                        if (item.LicenseRequired)
                         {
-                            if (licenseValidation.IsValid(license))
+                            // set default to false
+                            item.IsValid = false;
+                            var licenses = licensesList.Where(i => i.ProductId == item.ProductId);
+                            // if valid license for current proudct if found, set IsValid to true
+                            foreach (var license in licenses)
                             {
-                                item.IsValid = true;
-                                break;
+                                if (licenseValidation.IsValid(license))
+                                {
+                                    item.IsValid = true;
+                                    break;
+                                }
                             }
                         }
-                    }
-                    else
-                    {
-                        // if license isn't required, consider product valid
-                        item.IsValid = true;
+                        else
+                        {
+                            // if license isn't required, consider product valid
+                            item.IsValid = true;
+                        }
                     }
                 }
-            }
 
-            // group gathered data into final model
-            var queryOuter = from f in facilities
-                        where f.Id == (int)key
-                        join qi in queryInner on f.Id equals qi.Key
-                        select new FacilityModel
-                        {
-                            Id = f.Id,
-                            Latitude = f.Latitude,
-                            Longitude = f.Longitude,
-                            Name = f.Name,
-                            Products = qi.ToList()
-                        };
+                // group gathered data into final model
+                queryOuter = from f in facilities
+                                 where f.Id == (int)key
+                                 join qi in queryInner on f.Id equals qi.Key
+                                 select new FacilityModel
+                                 {
+                                     Id = f.Id,
+                                     Latitude = f.Latitude,
+                                     Longitude = f.Longitude,
+                                     Name = f.Name,
+                                     Products = qi.ToList()
+                                 };
+            }
+            else
+            {
+                queryOuter = from f in facilities
+                                 where f.Id == (int)key
+                                 select new FacilityModel
+                                 {
+                                     Id = f.Id,
+                                     Latitude = f.Latitude,
+                                     Longitude = f.Longitude,
+                                     Name = f.Name,
+                                     Products = new List<FacilitiesProductContainer>()
+                                 };
+
+            }
 
             return queryOuter.Single();
         }
