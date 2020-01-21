1573cd90b7a1363debd37db79e28674e0bf650bc
42ama <45532431+42ama@users.noreply.github.com>
2020-01-21 14:12:51 +0400

    Added related entities validation to License section: Controller, LogicValidation and Tests.

--- Cursed/Models/Business/LogicValidation/LicensesLogicValidation.cs
+++ Cursed/Models/Business/LogicValidation/LicensesLogicValidation.cs
@@ -4,6 +4,7 @@ using Cursed.Models.Context;
 using Cursed.Models.DataModel.ErrorHandling;
 using Cursed.Models.StaticReferences.Routing;
 using Cursed.Models.Services;
+using Cursed.Models.Entities.Data;
 
 namespace Cursed.Models.LogicValidation
 {
@@ -45,11 +46,30 @@ namespace Cursed.Models.LogicValidation
         /// <summary>
         /// Checks if license is valid, to be updated
         /// </summary>
-        /// <param name="key">Id of license to be found</param>
+        /// <param name="model">License model to be validated</param>
         /// <returns>Status message with validaton information</returns>
-        public async Task<IErrorHandler> CheckUpdateDataModelAsync(object key)
+        public async Task<IErrorHandler> CheckUpdateDataModelAsync(License model)
         {
-            return await CheckExists(key);
+            var statusMessage = await CheckExists(model.Id);
+            return await CheckRelatedEntitiesExists(model, statusMessage);
+        }
+
+        /// <summary>
+        /// Checks if license is valid, to be added
+        /// </summary>
+        /// <param name="model">License model to be validated</param>
+        /// <returns>Status message with validaton information</returns>
+        public async Task<IErrorHandler> CheckAddDataModelAsync(License model)
+        {
+            var statusMessage = errorHandlerFactory.NewErrorHandler(new Problem
+            {
+                Entity = "License.",
+                EntityKey = "",
+                RedirectRoute = LicensesRouting.Index,
+                UseKeyWithRoute = false
+            });
+
+            return await CheckRelatedEntitiesExists(model, statusMessage);
         }
 
         /// <summary>
@@ -60,6 +80,29 @@ namespace Cursed.Models.LogicValidation
         public async Task<IErrorHandler> CheckRemoveDataModelAsync(object key)
         {
             return await CheckExists(key);
+        }
+
+        /// <summary>
+        /// Checks if related entities exitst
+        /// </summary>
+        /// <param name="model">License model with key properties of related entities</param>
+        /// <param name="statusMessage">Error handler to which problem will be added</param>
+        /// <returns>Status message with validaton information</returns>
+        private async Task<IErrorHandler> CheckRelatedEntitiesExists(License model, IErrorHandler statusMessage)
+        {
+            // check if related product from catalog exists
+            if (await db.ProductCatalog.FirstOrDefaultAsync(i => i.Id == model.ProductId) == null)
+            {
+                statusMessage.AddProblem(new Problem
+                {
+                    Entity = "Products from catalog.",
+                    EntityKey = (model.ProductId).ToString(),
+                    Message = "Product with this Id isn't found",
+                    RedirectRoute = ProductsCatalogRouting.Index,
+                    UseKeyWithRoute = false
+                });
+            }
+            return statusMessage;
         }
 
         /// <summary>
